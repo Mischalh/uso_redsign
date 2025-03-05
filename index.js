e0dba727196e0d3a364efb2a440bcf8b8b888348
Mischalh <mischalh2003@gmail.com>
2025-03-05 21:04:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function dienstenBtn() {
+    window.location.href = "diensten.pagina/diensten.html";
+}
+
+let heroBlockDropdown = document.querySelector("#hero-block-link-ons-werk")
+
+heroBlockDropdown.addEventListener("click", function() {
+  
+})
+
+heroBlockDropdown.addEventListener("click", function() {
+    heroBlockDropdown.textContent += 
+    " Ons team beschikt over uitgebreide kennis en expertise in alle facetten van installatietechniek. Gevestigd in een karakteristiek pand aan de oostelijke ringweg in Groningen, hebben wij jarenlange ervaring in zowel eenvoudige als complexe elektrotechnische projecten."
+})
